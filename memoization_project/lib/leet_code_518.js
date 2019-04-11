65e684f9b0bf16f14244f91559a45d4d4cd2fc4e
Aram Sargsyan <aramsargsyan990@gmail.com>
2019-04-10 21:42:17 -0700

    Solve tabulation problems

--- memoization_project/lib/leet_code_518.js
+++ memoization_project/lib/leet_code_518.js
@@ -1,36 +1,33 @@
 // Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
-function countChange(a,c, memo) {
-    if (a === 0) return 0;
-    if (c.length === 0) return null; 
-    if (c[0] > a) return null;
-    if ( a % c[c.length - 1] === 0) memo[0]++;
-    let res
-    if (memo[ a % c[c.length - 1]]) {
-        res += memo[ a % c[c.length - 1]]
-    } else {
-        res = countChange( a % c[c.length - 1], c.slice(0, c.length - 1), memo)
+var change = function(amount, coins, memo = {0: 1}) {
+    if (amount === 0) return 1;
+    if ( !coins.length) return 0;
+    let c = []; 
+    for (let i = 0; i < coins.length; i++) {
+        c.push(coins[i]); 
+      for (let a = 0; a <= amount; a++) {
+          let newComb = countChange(a, c, memo);
+               memo[a] = newComb  
+          
+      }
     }
-    if (res) {
-        return memo[a] += res;
-    } else {
-        return null
-    }
-    
-}
-
-function minChange(coins, amount, memo = {}) {
-  let c = []; 
-  for (let i = 0; i < coins.length; i++) {
-      c.push(coins[i]); 
-    for (let a = 0; a <= amount; a++) {
-        let newComb = countChange(a, c, memo); 
+    return memo[amount]
+  };
   
-        if (memo[a]) {
-            memo[a] += newComb;
-        }
-    }
+  function countChange(a,c, memo) { 
+      if ( a === 0) return 1; 
+      if (c.length === 0) return 0; 
+      
+      memo[a] = memo[a] ? memo[a] : 0
+      let res;
+      if (memo[a - c[c.length - 1]]) {
+          res = memo[a - c[c.length - 1]]
+      } else {
+          res = countChange(a - c[c.length - 1], c.slice(0,c.length - 1), memo)
+          memo[a - c[c.length - 1]] = res;
+      }
+      
+      return memo[a] + res
   }
-  return memo[amount]
-}
