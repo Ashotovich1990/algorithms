4c792978826db6334ce8cb86b2ef30e83f196ce0
Aram Sargsyan <aramsargsyan990@gmail.com>
2019-04-11 10:35:34 -0700

    Solve min path sum problem

--- tabulation_project/lib/leet_code_64.js
+++ tabulation_project/lib/leet_code_64.js
@@ -2,5 +2,27 @@
 // Feel free to use this file for scratch work.
 
 function minPathSum(grid) {
+   let res = [];
+    for (let i = 0; i < grid.length; i++) {
+        res.push([]);
+    }
+   
+    res[0][0] = grid[0][0]
+    for (let i = 1; i < grid[0].length; i++) {
+        res[0][i] = res[0][i-1] + grid[0][i]
+    }
+    for (let i = 1; i < grid.length; i++) {
+        res[i][0] = res[i - 1][0] + grid[i][0]
+    }
+    
+    for (let i = 1; i < grid.length; i++) {
+        for (let j = 1; j < grid[i].length; j++ ) {
+            let optPath = res[i-1][j] < res[i][j-1] ? res[i-1][j] : res[i][j-1]; 
+            res[i][j] = optPath + grid[i][j];
+        }
+    }
+    
+    return res[res.length -1][res[0].length -1]
+    
 
 }
--- tabulation_project/lib/leet_code_70.js
+++ tabulation_project/lib/leet_code_70.js
@@ -1,6 +1,14 @@
 // Work through this problem on https://leetcode.com/problems/climbing-stairs/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
-function climbStairs(n) {
+function climbStairs(n, memo = {0 : 0, 1: 1, 2: 2}) {
+   if (memo[n]) return memo[n];
 
+   for (let i = 3; i <= n; i++) {
+       if (!memo[i]) {
+           memo[i] = climbStairs(i - 1, memo) + climbStairs(i - 2, memo)
+       }
+   }
+
+   return memo[n];
 }
