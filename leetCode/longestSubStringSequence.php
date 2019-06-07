b1381e1b4e2de42089fdc11a6ca6f2025ad33ec3
Aram Sargsyan <aramsargsyan990@gmail.com>
2019-06-07 13:50:37 -0700

    Solve non repeating str sliding window approach

--- leetCode/longestSubStringSequence.php
+++ leetCode/longestSubStringSequence.php
@@ -20,3 +20,27 @@ function longestPalindromeSubseq($s) {
         return $table[0][strlen($s) - 1];
         
     }
+
+
+    function lengthOfLongestSubstring($str) {
+        if (strlen($str) < 2) return strlen($str);
+        $res = 0; 
+        $i = 0; 
+        $j = 0; 
+        $set = [];
+        $l = strlen($str);
+        $sub_len = 0;
+        while ($i < $l && $j < $l) {
+            if (!$set[$str[$j]]) {
+                $set[$str[$j]] = true;
+                $sub_len++;
+                $j++;
+            } else {
+                $set[$str[$i]] = null;
+                $res = $res > $sub_len ? $res : $sub_len;
+                $sub_len--;
+                $i++;
+            }
+        }
+        return $res = $res > $sub_len ? $res : $sub_len;
+    }
--- leetCode/longestUniqueSubString.js
+++ leetCode/longestUniqueSubString.js
@@ -27,4 +27,3 @@ const longestUniqueSubStr = (str) => {
 };
 
 
-// xyzk
