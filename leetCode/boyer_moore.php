cc1efbf239a795bfa68b5fed1048b8601e33a901
Aram Sargsyan <aramsargsyan990@gmail.com>
2019-07-19 09:49:10 +0400

    boyer-moore alg in php for 2 el

--- leetCode/boyer_moore.php
+++ leetCode/boyer_moore.php
@@ -16,3 +16,47 @@ function majorityElement($nums) {
         
         return $res;
     }
+
+<!-- for 2 elements -->
+
+    function majorityElement($nums) {
+        $res = []; 
+         $res_fin = []; 
+        foreach($nums as $num) {
+            if (sizeof($res) !== 2) {
+                if (!$res[$num]) {
+                    $res[$num] = 1;
+                } else {
+                    $res[$num]++;
+                    if ($res[$num] > sizeof($nums)/3) $res_fin[$num] = $num;
+                };    
+            } else {
+                if ($res[$num]) {
+                    $res[$num]++;
+                } else {
+                    foreach($res as $key =>$val) {
+                        $res[$key]--;
+                        if ($val === 0) {
+                            unset($res[$key]);
+                            $res[$num] = 1;
+                           
+                        }
+                    };
+                }
+            };
+            
+           
+        };
+       
+      
+        foreach($res as $key => $val) {
+            $val = 0; 
+            foreach($nums as $num) {
+                if ($key === $num) $val++;
+            };
+            if ($val > sizeof($nums)/3) $res_fin[$key] = $key;
+        };
+        
+        return $res_fin;
+        
+    }
