014164a7c8e37b76174b0cfaa78e3b0dc73b4e55
Aram Sargsyan <aramsargsyan990@gmail.com>
2019-08-29 13:52:39 +0400

    is linked list polindrome in php

---
+++ leetCode/isLinkedListPalindrome.php
@@ -0,0 +1,22 @@
+class Solution {
+
+/**
+ * @param ListNode $head
+ * @return Boolean
+ */
+function isPalindrome($head) {
+    $reversed = [];
+    $node = $head;
+    while ($node) {
+       array_unshift($reversed, $node->val); 
+       $node = $node->next;
+    }; 
+    $size = sizeof($reversed);
+    for ($i = 0; $i < floor($size/2); $i++) {
+        if ($reversed[$size - 1 - $i] !== $reversed[$i]) return false;
+        $node = $node->next;
+    };
+    
+    return true;
+}
+}
