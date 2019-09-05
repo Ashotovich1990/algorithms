8ca24d756290e14d7f89f2b0b723922440722c6a
Aram Sargsyan <aramsargsyan990@gmail.com>
2019-09-05 09:51:46 +0400

    optimize next larger node in js

--- leetCode/nextLargerNode.js
+++ leetCode/nextLargerNode.js
@@ -1,28 +1,50 @@
-var nextLargerNodes = function(head) { 
-    let values = [];
+const nextLargerNodes = function (head) {
+    let res = [];
+    let stack = [];
     let node = head;
+    let i = 0;
     while (node) {
-        values.push(node.val);
+        while (stack.length && node.val > stack[stack.length-1].val) {
+            res[stack.pop().index] = node.val;
+        }; 
+        stack.push({
+            'index' : i,
+             'val' : node.val
+        });
         node = node.next;
+        i++;
     }; 
-    
-    let res = new Array(values.length).fill(0);
-    let i = 1; 
-    let j = 0;
-    while (j < values.length) {
-        i = j+1;
-        while (i < values.length) {
-           if (values[i] > values[j]) {
-               res[j] = values[i];
-               j++;
-               i = j+1;
-           } else {
-               i++;
-           }
-        };
-        j++;
-    }
-    
+    stack.forEach(el => res[el.index] = 0);
     return res;
+}; 
+
+
+
+// var nextLargerNodes = function(head) { 
+//     let values = [];
+//     let node = head;
+//     while (node) {
+//         values.push(node.val);
+//         node = node.next;
+//     }; 
     
-};
+//     let res = new Array(values.length).fill(0);
+//     let i = 1; 
+//     let j = 0;
+//     while (j < values.length) {
+//         i = j+1;
+//         while (i < values.length) {
+//            if (values[i] > values[j]) {
+//                res[j] = values[i];
+//                j++;
+//                i = j+1;
+//            } else {
+//                i++;
+//            }
+//         };
+//         j++;
+//     }
+    
+//     return res;
+    
+// };
