6c7e0499910e9f6a7fa0b2ddca89dab1b32129e4
Aram Sargsyan <aramsargsyan990@gmail.com>
2019-09-26 16:43:56 +0400

    solve sum root to leaf in java

---
+++ leetCode/sumRootToLeaf.java
@@ -0,0 +1,43 @@
+class Solution {
+    ArrayList<String> paths = new ArrayList<String>();
+    public int sumNumbers(TreeNode root) {
+        paths = this.traverse(root);
+        int res = 0;
+        int i = 0;
+        
+        while (i < paths.size()) {
+            res += Integer.parseInt(paths.get(i));
+            i++;
+        }
+        return res;
+    };
+    
+    public ArrayList<String> traverse(TreeNode root) {
+        ArrayList<String> res = new ArrayList<String>();
+        // res.add('');
+        if (root != null) {
+            if (root.left == null && root.right == null) {
+                res.add(Integer.toString(root.val));
+                return res;
+            } else if (root.right == null) {
+                res = traverse(root.left); 
+                res.replaceAll(s -> Integer.toString(root.val).concat(s));
+                return res;
+            } else if (root.left == null) {
+                res = traverse(root.right); 
+                res.replaceAll(s -> Integer.toString(root.val).concat(s));
+                return res;
+            } else {
+                ArrayList<String> left = traverse(root.left); 
+                left.replaceAll(s -> Integer.toString(root.val).concat(s));
+                    
+                ArrayList<String> right = traverse(root.right); 
+                right.replaceAll(s -> Integer.toString(root.val).concat(s));
+                
+                left.addAll(right);
+                return left;
+            }
+        };
+        return res;
+    };
+};
