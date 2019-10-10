92a0d1b4bcefb2db7d2e535367872c66e1446029
Aram Sargsyan <aramsargsyan990@gmail.com>
2019-10-10 19:17:41 +0400

    solve min cost climbing stairs in swift

---
+++ leetCode/746.swift
@@ -0,0 +1,16 @@
+class Solution {
+    var memo: [Int: Int] = [:]
+    
+    func minCostClimbingStairs(_ cost: [Int], stair: Int = 0) -> Int {
+        if stair >= cost.count - 1 { return 0 }
+       
+        if let result = self.memo[stair] {
+            return result
+        } else {
+            var takeOneStep = cost[stair] + self.minCostClimbingStairs(cost, stair: stair + 1)
+            var takeTwoStep = cost[stair+1] + self.minCostClimbingStairs(cost, stair: stair + 2)
+            self.memo[stair] = min(takeOneStep, takeTwoStep)
+            return memo[stair]!
+        }
+    }
+}
